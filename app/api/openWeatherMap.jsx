fbad32a68d2061592701eb5f20c1ef3edb7ac9b7
Alistair Willis <alistairrwillis@gmail.com>
2017-01-16 19:53:26 +0900

    test

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,6 +1,6 @@
 var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?units=metric&appid=ba02317420a71b482c575129ae75f584'; 
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=ba02317420a71b482c575129ae75f584&units=metric'; 
  
 module.exports = {
     getTemp: function (location) {
