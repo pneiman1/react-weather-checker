befd2727295addae54afc32cc073891c595b43c5
Alistair Willis <alistairrwillis@gmail.com>
2017-01-16 18:38:50 +0900

    App functionality complete.

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require('express'); 
 
 var app = express(); 
+const PORT = process.env.PORT || 3000; 
 
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+app.use(function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        next(); 
+    } else {
+        res.redirect('https://' + req.hostname + req.url);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log('Server running on port...' + PORT);
 }); 
