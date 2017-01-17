5a6e971cd5c4fb2f1b10c7afa65452da272c1f19
Alistair Willis <alistairrwillis@gmail.com>
2017-01-17 19:42:01 +0900

    Fixed search function

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -3,7 +3,15 @@ var {Link, IndexLink} = require('react-router');
 
 var Nav = React.createClass ({
     onSearch: function (e) {
-        alert('not connected');
+        e.preventDefault(); 
+        var location = this.refs.search.value; 
+
+        var encodedLocation = encodeURIComponent(location);
+
+        if (location && location.length > 0) {
+            this.refs.search.value = '';
+            window.location.hash = '#/?location=' + encodedLocation; 
+        }
     },     
     render: function () {
         return(
@@ -26,7 +34,7 @@ var Nav = React.createClass ({
                     <form onSubmit={this.onSearch}>
                         <ul className="menu">
                             <li>
-                                <input type="search" placeholder="Search weather by city"/>
+                                <input type="search" placeholder="Search weather by city" ref='search'/>
                             </li>
                             <li>
                                 <input type="submit" className="button" value="Get Weather"/>
