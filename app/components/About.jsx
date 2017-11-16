3e1919e76fcfb1a9b5f322c30df64cf324afb272
Phillip Neiman <pneiman1@gmail.com>
2017-11-15 16:27:13 -0800

    About Update

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -8,7 +8,7 @@ var About = (props) => {
             <div medium-4 large-3 small-centered>
                 <h1 className="text-center page-title">About</h1>
                 <p>This weather application is built using React.js and Foundation. To view the source code, visit:</p>
-                <a href='https://github.com/ARWL2016/react-weather' target="_blank">Github React-Weather</a>
+                <a href='https://github.com/pneiman1/react-weather-checker' target="_blank">Github React-Weather-Checker</a>
             </div>
         </div>
         )
